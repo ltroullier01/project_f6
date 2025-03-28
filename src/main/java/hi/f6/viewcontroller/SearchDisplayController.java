ee07d7def5c3128cb9b27d5872eb3b5943ea7553
ltroullier01 <lael.troullier@orange.fr>
2025-03-28 13:56:09 +0000

    Search Display Controller

--- src/main/java/hi/f6/viewcontroller/SearchDisplayController.java
+++ src/main/java/hi/f6/viewcontroller/SearchDisplayController.java
@@ -6,34 +6,42 @@ import hi.f6.gui.SearchDisplay;
 
 public class SearchDisplayController {
 
-    //private FlightController flightController;
+    // Innitialization of attributs
     private SearchDisplay searchDisplay;
+    //private FlightController flightController;
 
+    // Constructor
     public SearchDisplayController (SearchDisplay searchDisplay_var/*,FlightController flightController_var*/){
         this.searchDisplay = searchDisplay_var;
         //this.flightController = flightController_var;
     }
 
+    // Search methid depending on the different cases
     public void search(){
+
+        // Recovery of values
         String departureCity = this.searchDisplay.getTf_departureCity().getText();
         String arrivalCity = this.searchDisplay.getTf_destinationCity().getText();
         LocalDate departureDate = this.searchDisplay.getDp_departureTime().getValue();
         LocalDate arrivalDate = this.searchDisplay.getDp_arrivalTime().getValue();
 
-        if(departureCity == ""||departureCity.equals("From")){
-            //this.flightController.searchWithoutDepartureCity();
-            System.out.println("rien");
-        }
-        if(arrivalCity == ""||arrivalCity.equals("To")){
-            //this.flightController.searchWithoutArrivalCity();
-            System.out.println("rien");
-        }
+        // Verification of the case
         if((departureCity == ""||departureCity.equals("From"))&&(arrivalCity == ""||arrivalCity.equals("To"))){
             //this.flightController.searchWithoutDepartureAndArrivalCity();
+            System.out.println("Without Departure & Arrival");
+        }else if(departureCity == ""||departureCity.equals("From")){
+            //this.flightController.searchWithoutDepartureCity();
+            System.out.println("Without Departure");
+            System.out.println("Destination : "+arrivalCity);
+        }else if(arrivalCity == ""||arrivalCity.equals("To")){
+            //this.flightController.searchWithoutArrivalCity();
+            System.out.println("Without Arrival");
+            System.out.println("Departure : "+departureCity);
+        } else {
+            //this.flightController.search();
+            System.out.println("Departure : "+departureCity);
+            System.out.println("Destination : "+arrivalCity);
         }
-        
-        System.out.println(departureCity);
-        System.out.println(arrivalCity);
         System.out.println(departureDate);
         System.out.println(arrivalDate);
     }
