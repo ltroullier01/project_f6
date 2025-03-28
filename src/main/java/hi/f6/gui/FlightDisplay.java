c77e25b860c3379d5eaa334e7e7c400ab075a64f
Clwrnd <127970668+Clwrnd@users.noreply.github.com>
2025-03-28 11:10:14 +0000

    conflict resolve

--- src/main/java/hi/f6/gui/FlightDisplay.java
+++ src/main/java/hi/f6/gui/FlightDisplay.java
@@ -2,6 +2,9 @@ package hi.f6.gui;
 
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 public class FlightDisplay extends VBox {
 
