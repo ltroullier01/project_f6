12f4651db1a5f3e3ad02e5605e713ccfad63fa4b
Clwrnd <127970668+Clwrnd@users.noreply.github.com>
2025-03-30 16:16:17 +0000

    fix error preparedStatement

--- src/main/java/hi/f6/database/FlightDB.java
+++ src/main/java/hi/f6/database/FlightDB.java
@@ -20,41 +20,44 @@ public class FlightDB {
         this.con = con;
     }
 
-    public List<Flight> search(String departureCity, String arrivalCity, LocalDate departureDate, LocalDate arrivalDate){
+    public List<Flight> search(String departureCity, String arrivalCity, LocalDate departureDate,
+            LocalDate arrivalDate) {
         List<Flight> listeFlight = new ArrayList<>();
 
-        String select = "SELECT id, ref, departure_city, destination_city, layover, departure_time, arrival_time, price, duration, carbon_footprint " +
-                    "FROM flights " +
-                    "WHERE departureCity = ? AND destinationCity = ? " +
-                    "AND departureTime >= ? AND arrivalTime <= ?";    
-        
+        String select = "SELECT id, ref, departure_city, destination_city, layover, departure_time, arrival_time, price, duration, carbon_footprint "
+                +
+                "FROM flights " +
+                "WHERE departure_city = ? AND destination_city = ? " +
+                "AND departure_time = ? AND arrival_time = ?";
+
         try (PreparedStatement pstmt = this.con.prepareStatement(select)) {
 
             pstmt.setString(1, departureCity);
             pstmt.setString(2, arrivalCity);
-            pstmt.setString(3, departureDate.toString()); 
+            pstmt.setString(3, departureDate.toString());
             pstmt.setString(4, arrivalDate.toString());
-            
-            ResultSet rs = pstmt.executeQuery(select);
-            
+
+            ResultSet rs = pstmt.executeQuery();
+
             while (rs.next()) {
                 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
                 dtf = dtf.withLocale(Locale.FRENCH);
 
-                int flightID = rs.getInt("flightID");
-                String flightRef = rs.getString("flightRef");
-                String departureCity_var = rs.getString("departureCity");
-                String destinationCity_var = rs.getString("destinationCity");
-                int layovers = rs.getInt("layovers");
-                String departureTimeString = rs.getString("departureTime");
+                int flightID = rs.getInt("id");
+                String flightRef = rs.getString("ref");
+                String departureCity_var = rs.getString("departure_city");
+                String destinationCity_var = rs.getString("destination_city");
+                int layovers = rs.getInt("layover");
+                String departureTimeString = rs.getString("departure_time");
                 LocalDate departureTime = LocalDate.parse(departureTimeString);
-                String arrivalTimeString = rs.getString("arrivalTime");
+                String arrivalTimeString = rs.getString("arrival_time");
                 LocalDate arrivalTime = LocalDate.parse(arrivalTimeString);
                 float price = rs.getFloat("price");
                 int duration = rs.getInt("duration");
-                float carbonFootprint = rs.getInt("carbonFootprint");
-                
-                Flight flight = new Flight(flightID, flightRef,departureCity_var,destinationCity_var,departureTime,arrivalTime,price,duration,layovers,carbonFootprint);
+                float carbonFootprint = rs.getInt("carbon_footprint");
+
+                Flight flight = new Flight(flightID, flightRef, departureCity_var, destinationCity_var, departureTime,
+                        arrivalTime, price, duration, layovers, carbonFootprint);
                 listeFlight.add(flight);
             }
 
@@ -65,19 +68,20 @@ public class FlightDB {
         return listeFlight;
     }
 
-    public List<Flight> searchWithoutDepartureCity(String departureCity, LocalDate departureDate, LocalDate ArrivalDate){
+    public List<Flight> searchWithoutDepartureCity(String departureCity, LocalDate departureDate,
+            LocalDate ArrivalDate) {
         List<Flight> listeFlight = new ArrayList<>();
 
         return listeFlight;
     }
 
-    public List<Flight> searchWithoutArrivalCity(String arrivalCity, LocalDate departureDate, LocalDate ArrivalDate){
+    public List<Flight> searchWithoutArrivalCity(String arrivalCity, LocalDate departureDate, LocalDate ArrivalDate) {
         List<Flight> listeFlight = new ArrayList<>();
 
         return listeFlight;
     }
 
-    public List<Flight> searchWithoutDepartureAndArrivalCity(LocalDate departureDate, LocalDate ArrivalDate){
+    public List<Flight> searchWithoutDepartureAndArrivalCity(LocalDate departureDate, LocalDate ArrivalDate) {
         List<Flight> listeFlight = new ArrayList<>();
 
         return listeFlight;
