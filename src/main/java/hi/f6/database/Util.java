f37b5bd8286fad9957f7298101cbe328d6eb8722
Clwrnd <127970668+Clwrnd@users.noreply.github.com>
2025-03-30 15:03:39 +0000

    .

--- src/main/java/hi/f6/database/Util.java
+++ src/main/java/hi/f6/database/Util.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/nom_de_la_base";
-    private static final String USER = "root";
-    private static final String PASSWORD = "mot_de_passe";
+    private static final String URL = "jdbc:mysql://localhost:3306/team6f";
+    private static final String USER = "clement";
+    private static final String PASSWORD = "clement";
 
     public Connection getConnection() {
         try {
